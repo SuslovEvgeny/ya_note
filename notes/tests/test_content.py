5f6f15e7e8e0eb97f564db553eacf926d66f9945
Evgeny Suslov <suslov.evgeny@gmail.com>
2023-10-26 21:31:48 +0300

    after home

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -7,98 +7,111 @@ from django.test import TestCase
 from django.urls import reverse
 from django.utils import timezone
 
-from news.models import News
+from notes.models import Note
 
 ...
 # Дополнительно к News импортируем модель комментария.
-from news.models import Comment, News
+from notes.models import Note
 
 User = get_user_model()
 
 
-class TestHomePage(TestCase):
-    HOME_URL = reverse('news:home')
+class TestNotesPage(TestCase):
 
     @classmethod
     def setUpTestData(cls):
-        # Вычисляем текущую дату.
-        today = datetime.today()
-        all_news = [
-            News(
-                title=f'Новость {index}',
-                text='Просто текст.',
-                # Для каждой новости уменьшаем дату на index дней от today,
-                # где index - счётчик цикла.
-                date=today - timedelta(days=index)
-            )
-            for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
-        ]
-        News.objects.bulk_create(all_news) 
-
-    def test_news_count(self):
-        # Загружаем главную страницу.
-        response = self.client.get(self.HOME_URL)
-        # Код ответа не проверяем, его уже проверили в тестах маршрутов.
-        # Получаем список объектов из словаря контекста.
-        object_list = response.context['object_list']
-        # Определяем длину списка.
-        news_count = len(object_list)
-        # Проверяем, что на странице именно 10 новостей.
-        self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE) 
-
-    def test_news_order(self):
-        response = self.client.get(self.HOME_URL)
-        object_list = response.context['object_list']
-        # Получаем даты новостей в том порядке, как они выведены на странице.
-        all_dates = [news.date for news in object_list]
-        # Сортируем полученный список по убыванию.
-        sorted_dates = sorted(all_dates, reverse=True)
-        # Проверяем, что исходный список был отсортирован правильно.
-        self.assertEqual(all_dates, sorted_dates)
-
-
-class TestDetailPage(TestCase):
-
-    @classmethod
-    def setUpTestData(cls):
-        cls.news = News.objects.create(
-            title='Тестовая новость', text='Просто текст.'
+        cls.author = User.objects.create(username='Автор')
+        cls.notes = Note.objects.create(
+            title='Тестовая новость', text='Просто текст.', slug='slug', author=cls.author
         )
-        # Сохраняем в переменную адрес страницы с новостью:
-        cls.detail_url = reverse('news:detail', args=(cls.news.id,))
-        cls.author = User.objects.create(username='Комментатор')
-        # Запоминаем текущее время:
-        now = timezone.now()
-        # Создаём комментарии в цикле.
-        for index in range(2):
-            # Создаём объект и записываем его в переменную.
-            comment = Comment.objects.create(
-                news=cls.news, author=cls.author, text=f'Tекст {index}',
-            )
-            # Сразу после создания меняем время создания комментария.
-            comment.created = now + timedelta(days=index)
-            # И сохраняем эти изменения.
-            comment.save()
-        
-    def test_comments_order(self):
-        response = self.client.get(self.detail_url)
-        # Проверяем, что объект новости находится в словаре контекста
-        # под ожидаемым именем - названием модели.
-        self.assertIn('news', response.context)
-        # Получаем объект новости.
-        news = response.context['news']
-        # Получаем все комментарии к новости.
-        all_comments = news.comment_set.all()
-        # Проверяем, что время создания первого комментария в списке
-        # меньше, чем время создания второго.
-        self.assertLess(all_comments[0].created, all_comments[1].created)
+        cls.notes_edit_url = reverse('notes:edit', args=(cls.notes.slug,))
+        cls.notes_create_url = reverse('notes:add')
 
-    def test_anonymous_client_has_no_form(self):
-        response = self.client.get(self.detail_url)
-        self.assertNotIn('form', response.context)
-        
     def test_authorized_client_has_form(self):
-        # Авторизуем клиент при помощи ранее созданного пользователя.
         self.client.force_login(self.author)
-        response = self.client.get(self.detail_url)
+        response = self.client.get(self.notes_edit_url)
         self.assertIn('form', response.context)
+        response = self.client.get(self.notes_create_url)
+        self.assertIn('form', response.context)
+    
+    def test_obj_list_in_context(self):
+        self.client.force_login(self.author)
+        response = self.client.get(self.notes_edit_url)
+        object_list = response.context['object_list']
+        self.assertIn(object_list, settings.NEWS_COUNT_ON_HOME_PAGE) 
+
+    # def test_news_order(self):
+    #     response = self.client.get(self.HOME_URL)
+    #     object_list = response.context['object_list']
+    #     all_dates = [news.date for news in object_list]
+    #     sorted_dates = sorted(all_dates, reverse=True)
+    #     self.assertEqual(all_dates, sorted_dates)
+
+
+
+
+
+        
+
+# class TestHomePage(TestCase):
+#     HOME_URL = reverse('news:home')
+
+#     @classmethod
+#     def setUpTestData(cls):
+#         today = datetime.today()
+#         all_news = [
+#             News(
+#                 title=f'Новость {index}',
+#                 text='Просто текст.',
+#                 date=today - timedelta(days=index)
+#             )
+#             for index in range(settings.NEWS_COUNT_ON_HOME_PAGE + 1)
+#         ]
+#         News.objects.bulk_create(all_news) 
+
+    # def test_news_count(self):
+    #     response = self.client.get(self.HOME_URL)
+    #     object_list = response.context['object_list']
+    #     news_count = len(object_list)
+    #     self.assertEqual(news_count, settings.NEWS_COUNT_ON_HOME_PAGE) 
+
+    # def test_news_order(self):
+    #     response = self.client.get(self.HOME_URL)
+    #     object_list = response.context['object_list']
+    #     all_dates = [news.date for news in object_list]
+    #     sorted_dates = sorted(all_dates, reverse=True)
+    #     self.assertEqual(all_dates, sorted_dates)
+
+
+# class TestDetailPage(TestCase):
+
+#     @classmethod
+#     def setUpTestData(cls):
+#         cls.news = News.objects.create(
+#             title='Тестовая новость', text='Просто текст.'
+#         )
+#         cls.detail_url = reverse('news:detail', args=(cls.news.id,))
+#         cls.author = User.objects.create(username='Комментатор')
+#         now = timezone.now()
+#         for index in range(2):
+#             comment = Comment.objects.create(
+#                 news=cls.news, author=cls.author, text=f'Tекст {index}',
+#             )
+#             comment.created = now + timedelta(days=index)
+#             comment.save()
+        
+#     def test_comments_order(self):
+#         response = self.client.get(self.detail_url)
+#         self.assertIn('news', response.context)
+#         news = response.context['news']
+#         all_comments = news.comment_set.all()
+#         self.assertLess(all_comments[0].created, all_comments[1].created)
+
+#     def test_anonymous_client_has_no_form(self):
+#         response = self.client.get(self.detail_url)
+#         self.assertNotIn('form', response.context)
+        
+#     def test_authorized_client_has_form(self):
+#         self.client.force_login(self.author)
+#         response = self.client.get(self.detail_url)
+#         self.assertIn('form', response.context)

e94b6b8e8377562d4aeed6481361b49dd30f651b
Evgeny Suslov <suslov.evgeny@gmail.com>
2023-10-31 00:02:24 +0300

    After review.

--- notes/tests/test_content.py
+++ notes/tests/test_content.py
@@ -25,6 +25,7 @@ class TestNotesPage(TestCase):
         )
 
     def test_pages_contains_form(self):
+        """Наличие формы на страницах редактирования и добавления."""
         urls = (
             ('notes:edit', (self.notes.slug,)),
             ('notes:add', None),
@@ -33,16 +34,24 @@ class TestNotesPage(TestCase):
             with self.subTest(name=name):
                 url = reverse(name, args=args)
                 response = self.auth_author.get(url)
-                self.assertIn('form', response.context)
+                self.assertIn('form', response.context,
+                              f'Убедитесь, что на страницу {url}'
+                              ' передается форма.')
 
     def test_notes_list_for_author(self):
+        """Заметка автора выводится на страницу."""
         url = reverse('notes:list')
         response = self.auth_author.get(url)
         object_list = response.context['object_list']
-        self.assertIn(self.notes, object_list)
+        self.assertIn(self.notes, object_list,
+                      f'Убедитесь, что на страницу {url}'
+                      ' выводится заметка автора.')
 
     def test_notes_list_for_other_author(self):
+        """Заметка автора не выводится на страницу другого."""
         url = reverse('notes:list')
         response = self.auth_reader.get(url)
         object_list = response.context['object_list']
-        self.assertNotIn(self.notes, object_list)
+        self.assertNotIn(self.notes, object_list,
+                         f'Убедитесь, что на страницу {url}'
+                         ' не выводится заметка другого автора.')
